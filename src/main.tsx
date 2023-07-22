21010ecbd054d2a015a3094eeeffc449a217e119
Jonny Pickard <Jonathandpickard@gmail.com>
2023-07-22 16:46:25 +0100

    fix: remove little state machine devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { StateMachineProvider, createStore } from "little-state-machine";
-import { DevTool } from "little-state-machine-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
@@ -15,7 +14,6 @@ createStore(clientSidePanelListStore);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <StateMachineProvider>
-      {process.env.NODE_ENV !== "production" && <DevTool />}
       <ChakraProvider theme={extendedTheme}>
         <NewPanel />
       </ChakraProvider>
