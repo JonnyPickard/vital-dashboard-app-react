e6bf867ec420a243b10fbc571a520d82059bacd8
Jonny Pickard <Jonathandpickard@gmail.com>
2023-07-26 11:05:44 +0100

    Final table cleanup steps

--- src/services/useLabTests.ts
+++ src/services/useLabTests.ts
@@ -1,17 +1,14 @@
 import axios, { AxiosError } from "axios";
 import { useQuery } from "react-query";
 
-import { buildLabTestsResponseMockData } from "../tests/mocks/labTestsResponseMockData";
 import type { LabTestsResponseData } from "../types/LabTestsResponseData.ts";
 
 export function useLabTests() {
   return useQuery<LabTestsResponseData, AxiosError>("labTests", async () => {
-    // TODO: Currently set as this so I don't spam the api while developing
-    return Promise.resolve(buildLabTestsResponseMockData(30));
-    // const { data } = await axios.get<LabTestsResponseData>(
-    //   "/lab_tests/markers",
-    // );
+    const { data } = await axios.get<LabTestsResponseData>(
+      "/lab_tests/markers",
+    );
 
-    // return data;
+    return data;
   });
 }
